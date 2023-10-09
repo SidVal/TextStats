9059340c83b69b7856b1145da9eacc0eac56b322
SidV <SidVal@users.noreply.github.com>
2023-10-09 06:35:07 -0300

    Update contar.js

--- assets/js/contar.js
+++ assets/js/contar.js
@@ -18,60 +18,71 @@ document.addEventListener("DOMContentLoaded", function () {
 
         let stopWords;
 
-        fetch('https://sidval.github.io/TextStats/assets/stops/words.json')
-            .then(response => response.json())
-            .then(data => {
-                stopWords = new Set(data.map(word => word.toLowerCase()));
-
-                // Filtrar palabras comunes y verbos en un solo bucle
-                const palabrasFiltradas = palabras.filter(palabra => {
-                    const palabraLower = palabra.toLowerCase();
-                    return !stopWords.has(palabraLower) && !stopVerbs.includes(palabraLower);
-                });
-
-                // Resto del código para calcular y mostrar estadísticas
-                // Contador de palabras
-                const totalPalabras = palabrasFiltradas.length;
-
-                // Cantidad de caracteres (con espacios)
-                const caracteresConEspacios = texto.length;
-                control = caracteresConEspacios;
-
-                // Cantidad de caracteres (sin espacios)
-                const caracteresSinEspacios = texto.replace(/\s/g, "").length;
-
-                // Calcular la densidad de palabras clave para cada palabra clave
-                const palabrasClave = calcularPalabrasClave(palabrasFiltradas, 10);
-                const densidadPalabrasClave = calcularDensidadPalabrasClave(palabrasFiltradas, palabrasClave);
-
-                //v2: indexPalabras
-                    // Eliminar signos y espacios en blanco y obtener palabras restantes
-                    const palabrasSinSignos = texto.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]+/g, " ");
-                    const palabrasIndex = palabrasSinSignos.trim().split(" ");
-
-                    // Actualizar el contador de índice de palabras
-                    const indexPalabras = palabrasIndex.length;
-                //v2 fin
-
-                //v3 Verificar si el div de estadísticas SEO está vacío
-                if (control == 0) {
-                seoStatsContainer.style.display = "none";
-                } else {
-                seoStatsContainer.style.display = "block";
-                }
-                //v3 fin
-
-                // Mostrar las estadísticas
-                seoStatsContainer.innerHTML = `
-                    <p>Index de palabras: ${indexPalabras}</p>
-                    <p>Palabras bajo análisis: ${totalPalabras}</p>
-                    <p>(${palabrasClave.length}) Palabras repetidas: ${palabrasClave.join(", ")}</p>
-                    ${palabrasClave.map((palabra, index) => `<p>Densidad de "${palabra}": ${densidadPalabrasClave[index].toFixed(2)}%</p>`).join("")}
-                    <p>Caracteres (con espacios): ${caracteresConEspacios}</p>
-                    <p>Caracteres (sin espacios): ${caracteresSinEspacios}</p>
-                `;
+            // Cargar el archivo "verbs.json"
+            fetch('ruta/verbs.json') // Reemplaza 'ruta' con la ruta correcta hacia tu archivo
+            .then(response1 => response1.json())
+            .then(data1 => {
+            stopVerbs = new Set(data1.map(verb => verb.toLowerCase()));
+            
+            // Ahora tienes cargada la lista de verbos en la variable stopVerbs
+            
+            fetch('https://sidval.github.io/TextStats/assets/stops/words.json')
+                        .then(response2 => response2.json())
+                        .then(data2 => {
+                                stopWords = new Set(data2.map(word => word.toLowerCase()));
+                
+                                // Filtrar palabras comunes y verbos en un solo bucle
+                                const palabrasFiltradas = palabras.filter(palabra => {
+                                    const palabraLower = palabra.toLowerCase();
+                                    return !stopWords.has(palabraLower) && !stopVerbs.includes(palabraLower);
+                                });
+                
+                                // Resto del código para calcular y mostrar estadísticas
+                                // Contador de palabras
+                                const totalPalabras = palabrasFiltradas.length;
+                
+                                // Cantidad de caracteres (con espacios)
+                                const caracteresConEspacios = texto.length;
+                                control = caracteresConEspacios;
+                
+                                // Cantidad de caracteres (sin espacios)
+                                const caracteresSinEspacios = texto.replace(/\s/g, "").length;
+                
+                                // Calcular la densidad de palabras clave para cada palabra clave
+                                const palabrasClave = calcularPalabrasClave(palabrasFiltradas, 10);
+                                const densidadPalabrasClave = calcularDensidadPalabrasClave(palabrasFiltradas, palabrasClave);
+                
+                                //v2: indexPalabras
+                                    // Eliminar signos y espacios en blanco y obtener palabras restantes
+                                    const palabrasSinSignos = texto.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]+/g, " ");
+                                    const palabrasIndex = palabrasSinSignos.trim().split(" ");
+                
+                                    // Actualizar el contador de índice de palabras
+                                    const indexPalabras = palabrasIndex.length;
+                                //v2 fin
+                
+                                //v3 Verificar si el div de estadísticas SEO está vacío
+                                if (control == 0) {
+                                seoStatsContainer.style.display = "none";
+                                } else {
+                                seoStatsContainer.style.display = "block";
+                                }
+                                //v3 fin
+                
+                                // Mostrar las estadísticas
+                                seoStatsContainer.innerHTML = `
+                                    <p>Index de palabras: ${indexPalabras}</p>
+                                    <p>Palabras bajo análisis: ${totalPalabras}</p>
+                                    <p>(${palabrasClave.length}) Palabras repetidas: ${palabrasClave.join(", ")}</p>
+                                    ${palabrasClave.map((palabra, index) => `<p>Densidad de "${palabra}": ${densidadPalabrasClave[index].toFixed(2)}%</p>`).join("")}
+                                    <p>Caracteres (con espacios): ${caracteresConEspacios}</p>
+                                    <p>Caracteres (sin espacios): ${caracteresSinEspacios}</p>
+                                `;
+                        })
+                        .catch(error => console.error('Error al cargar el JSON:', error));
+                
             })
-            .catch(error => console.error('Error al cargar el JSON:', error));
+            .catch(error => console.error('Error al cargar el archivo JSON de verbos:', error));
     }
 
     // Escuchar cambios en el textarea y recalcular estadísticas
